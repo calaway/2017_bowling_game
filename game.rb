8dc0c663d411bf182e93a4fc922185f6195e999a
Calaway <calaway@users.noreply.github.com>
2017-06-27 23:53:53 -0600

    Calaway's Solution
    I'd like to see other people's solutions for how to calculate the extra points after a spare or strike. I imagine there is a more elegant way than what I have found. This was a great problem to work on. =)

---
+++ game.rb
@@ -0,0 +1,35 @@
+class Game
+  def initialize
+    @score = []
+  end
+
+  def reset
+    @score = []
+  end
+
+  def add_roll(pin_count)
+    @score << pin_count
+    @score << 0 if pin_count == 10
+  end
+
+  def score
+    @extra_points = @score.map.with_index do |pin_count, index|
+      if (index == 0) || (index == 1) # cannot recieve extra points on first frame or after 20 rolls
+        0
+      elsif ((4..19).include?(index)) && (@score[index - 2] == 10) && (@score[index - 4] == 10) # roll following two strikes
+        pin_count * 2
+      elsif (index < 22) && (@score[index - 2] == 10) && (index % 2 == 0) # roll following strike
+        pin_count
+      elsif (@score[index - 3] == 10) && (index % 2 == 1) # second roll following strike
+        pin_count
+      elsif index % 2 == 1 # second roll in frame not following strike
+        0
+      elsif ((@score[index - 1] + @score[index - 2]) == 10) && (index < 20) # roll following a spare
+        pin_count
+      else
+        0
+      end
+    end
+    @score.sum + @extra_points.sum
+  end
+end
